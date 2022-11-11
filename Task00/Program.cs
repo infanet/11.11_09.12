b86ebc40fb1770746d6081f36f17533cc2823098
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-11 20:09:52 +0400

    add ad

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -3,20 +3,13 @@
 // int number = Convert.ToInt32(Console.ReadLine());
 // int result = number * number;
 // Console.WriteLine($"Квадрат из {number} = {result}");
-// Console.WriteLine("Напиши квадратное число");
-// int number1 = Convert.ToInt32(Console.ReadLine());
-// Console.WriteLine("Напиши число относящиеся к первому числу возводя в квадрат");
-// int number2 = Convert.ToInt32(Console.ReadLine());
-// if(number1 == number2 * number2){
-//     Console.WriteLine("Первое число является квадратом второго числа");
-// }
-// else{
-//     Console.WriteLine("Первое число не является квадратом второго числа");
-// }
-int number = 4;
-int index = 0 - number;
-
-while(index <= number){
-System.Console.WriteLine(index);
-index++;
+Console.WriteLine("Напиши квадратное число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Напиши число относящиеся к первому числу возводя в квадрат");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 == number2 * number2){
+    Console.WriteLine("Первое число является квадратом второго числа");
+}
+else{
+    Console.WriteLine("Первое число не является квадратом второго числа");
 }
---
+++ Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 0 - number;
+if(number >= 0){
+while(index <= number){
+System.Console.WriteLine(index);
+index++;
+}
+}
+else{
+    while(index >= number){
+System.Console.WriteLine(index);
+index--;
+    }
+}
