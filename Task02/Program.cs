0fce7ca96d57a80b6f6c6207916700cd1ebb1099
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-11 20:24:34 +0400

    Add Task02

---
+++ Task02/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите Первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if(number1 > number2){
+    Console.WriteLine($"Большое число {number1} и малое число {number2}");
+}
+else{
+    Console.WriteLine($"Большое число {number2} и малое число {number1}");
+}
