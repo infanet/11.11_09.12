a2aa58a6c26d7dc29aecb010ccd745752f9ad646
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-11 20:35:01 +0400

    Добавил исправление

--- Task02/Program.cs
+++ Task02/Program.cs
@@ -5,6 +5,9 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 if(number1 > number2){
     Console.WriteLine($"Большое число {number1} и малое число {number2}");
 }
+else if(number1 == number2){
+    Console.WriteLine($"Число {number1} равно числу {number2}");
+}
 else{
     Console.WriteLine($"Большое число {number2} и малое число {number1}");
 }
