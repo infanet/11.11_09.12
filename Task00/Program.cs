0b43f72ec1016959de5534bc870af396fb3b8c42
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-11 19:28:31 +0400

    Add Task

--- Task00/Program.cs
+++ Task00/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Console.WriteLine("Ввеедите число");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int result = number * number;
+// Console.WriteLine($"Квадрат из {number} = {result}");
+// Console.WriteLine("Напиши квадратное число");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Напиши число относящиеся к первому числу возводя в квадрат");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// if(number1 == number2 * number2){
+//     Console.WriteLine("Первое число является квадратом второго числа");
+// }
+// else{
+//     Console.WriteLine("Первое число не является квадратом второго числа");
+// }
+int number = 4;
+int index = 0 - number;
+
+while(index <= number){
+System.Console.WriteLine(index);
+index++;
+}
--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Напиши число дня недели которое тебя интересует");
+int chislo = Convert.ToInt32(Console.ReadLine());
+if(chislo == 1){
+    Console.WriteLine("Это Понедельник");
+}
+else if(chislo == 2){
+    Console.WriteLine("Это Вторник");
+}
+else if(chislo == 3){
+    Console.WriteLine("Это Среда");
+}
+else if(chislo == 4){
+    Console.WriteLine("Это Четверг");
+}
+else if(chislo == 5){
+    Console.WriteLine("Это Пятница");
+}
+else if(chislo == 6){
+    Console.WriteLine("Это Суббота");
+}
+else if(chislo == 7){
+    Console.WriteLine("Это Воскресенье");
+}
+else{
+    Console.WriteLine("Не то написал");
+}
