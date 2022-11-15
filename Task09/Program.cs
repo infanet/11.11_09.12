1153051d07a5183ed900376baf37d3bacd78352b
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-15 11:57:41 +0400

    Add Task09

---
+++ Task09/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = new Random().Nex(10, 100);
+Console.WriteLine($"Случайное число из отрезка от 10 - 99 --> {number}");
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine($"Наибольшая цифра числа --> {firstDigit}");
+}
+else
+{
+    Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
+}
