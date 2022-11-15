8a877abdd6addeb755e32338cbeb28dedebbf704
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-15 13:34:31 +0400

    Add Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,8 @@
+﻿int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+bool Pod(int num1, int num2)
+{
+    return (num1 * num1 == num2) || (num2 * num2 == num1);
+}
+Pod(num1, num2);
+Console.WriteLine(Pod(num1, num2) ? "Да" : "Нет");
