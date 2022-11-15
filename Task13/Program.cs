24bf09b544d6c4f57decd20dd038b06ac2c7afca
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-15 18:01:09 +0400

    Task13

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string str = Console.ReadLine();
+float num = float.Parse(str);
+char Void(string str, int num)
+{
+    char[] ch = str.ToCharArray();
+    int i = 0;
+    for (; i <= ch.Length; i++)
+    {
+        if (i == num - 1)
+        {
+            break;
+        }
+    }
+    return ch[i];
+}
+bool Remainder(float rem)
+{
+    return rem / 100 < 0.9;
+}
+Console.WriteLine(Remainder(num) ? "Третьей цифры нет" : Void(str, 3));
