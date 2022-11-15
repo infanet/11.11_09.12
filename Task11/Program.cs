331343a23d43d8f224606767161584a5f94eb9a1
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-15 12:29:56 +0400

    Add Task11-12

---
+++ Task11/Program.cs
@@ -0,0 +1,12 @@
+﻿int ran = new Random().Nex(100, 1000);
+Console.WriteLine($"Случайное число из 100 - 1000 --> {ran}");
+
+int Pod(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    int result = firstDigit * 10 + secondDigit;
+    return result;
+}
+
+Console.WriteLine(Pod(number));
---
+++ Task12/Program.cs
@@ -0,0 +1,18 @@
+﻿int num1 = Convert.ToInt32(Console.ReadLine());
+int num2 = Convert.ToInt32(Console.ReadLine());
+int Pod(int num1, int num2)
+{
+    int os = 0;
+    if (num1 % num2 == 0)
+    {
+        Console.WriteLine("Кратно");
+    }
+    else
+    {
+        os = num1 % num2;
+        Console.WriteLine($"не кратно, остаток {os}");
+    }
+    return os;
+}
+
+Console.WriteLine(Pod(num1, num2));
