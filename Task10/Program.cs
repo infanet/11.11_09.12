81d94dfe884bd6520196b469c22fd51fc159cd5e
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-15 15:49:45 +0400

    Add Task10,13,15

---
+++ Task10/Program.cs
@@ -0,0 +1,9 @@
+﻿int ran = new Random().Next(100, 1000);
+Console.WriteLine($"Рандомное значное числа = {ran}");
+int Counting(int ran)
+{
+    int n1 = ran / 10;
+    int n2 = n1 % 10;
+    return n2;
+}
+Console.WriteLine(Counting(ran));
---
+++ Task15/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Введи цифру и я тебе скажу является ли он выходным");
+            int num = Convert.ToInt32(Console.ReadLine());
+            bool Weekends(int num)
+            {
+                bool n = true;
+                if (num == 1) n = false;
+                if (num == 2) n = false;
+                if (num == 3) n = false;
+                if (num == 4) n = false;
+                if (num == 5) n = false;
+                if (num == 6) n = true;
+                if (num == 7) n = true;
+                return n;
+            }
+            bool Week(int num)
+            {
+                return num > 7 || num <= 0;
+            }
+            Console.WriteLine(Week(num) ? "Такого дня недели нет" : Weekends(num) ? "Да" : "Нет");
