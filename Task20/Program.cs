0b566656df33d67c127be08e7ae6a0b514a1e8f7
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-18 12:39:59 +0400

    Add Task20

---
+++ Task20/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите координаты тички ХA");
+            int xa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координаты тички YA");
+            int ya = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите координаты тички ХB");
+            int xb = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите координаты тички YB");
+            int yb = Convert.ToInt32(Console.ReadLine());
+
+            double Quarter(int xa, int ya, int xb, int yb)
+            {
+                double xc = (xa - xb) * (xa - xb);
+                double yc = (ya - yb) * (ya - yb);
+                double result = Math.Sqrt(xc + yc);
+                return result;
+            }
+            double res = Quarter(xa, ya, xb, yb);
+            double resRound = Math.Round(res, 2, MidpointRounding.ToZero);
+            Console.WriteLine(resRound);
