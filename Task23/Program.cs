b6a7dcbdc14d21e5d517ae8bd5da9610eb06c250
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-20 16:56:43 +0400

    Add Task23

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+string str(int num)
+{
+    string stri = "";
+    for (int i = 1; i <= num; i++)
+    {
+        stri = stri + $"{i} | {i * i * i} \n";
+    }
+    return stri;
+}
+Console.WriteLine(str(num));
