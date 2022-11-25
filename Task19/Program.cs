d71031f91572ce1c1c65b62cf129a4dcd534ac5b
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-25 10:53:37 +0400

    Add Task19

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введи число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+void Palindrome(int n)
+{
+    int copiend_num = n;
+    int modified_num = 0;
+    int divided_num = 0;
+    while (n > 0)
+    {
+        divided_num = n % 10;
+        modified_num = modified_num * 10 + divided_num;
+        n = n / 10;
+    }
+    if (copiend_num == modified_num)
+    {
+        Console.WriteLine($"{copiend_num} -> да");
+    }
+    else
+    {
+        Console.WriteLine($"{copiend_num} -> нет");
+    }
+}
+
+Palindrome(num);
