881b40548982de46e01de3b8081f33c19487fcdb
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-30 00:36:04 +0400

    Add Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.Readline());
+
+int SumDigits(int num)
+{
+    int sum = default;
+    for (int i = 0; i <= num; i++)
+    {
+        sum = sum + i;
+    }
+
+    return sum ;
+}
+
+int sumDigits = SumDigits(number);
+Console.WriteLine($"Сумма чмсел от 1 до {number} = {sumDigits}");
