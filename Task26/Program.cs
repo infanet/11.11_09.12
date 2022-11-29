d65d68493921742fcc9147106534484e8f070dfe
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-30 00:59:24 +0400

    Add Task26

---
+++ Task26/Program.cs
@@ -0,0 +1,19 @@
+﻿System.Console.WriteLine("Введите число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int NumberLength(int num)
+{
+    int n = 1;
+    while (num > 0)
+    {
+        num = num /10;
+        if (num != 0)
+        {
+            n++;
+        }
+    }
+    return n;
+}
+
+int nmberLength = NumberLength(number);
+System.Console.WriteLine(nmberLength);
