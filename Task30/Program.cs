fc9358a09430df4231762cea899952b0920b61aa
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-30 01:25:18 +0400

    Add Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] array = new int[8];
+void RandomArray(int[] array)
+{
+    Random ran = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ran.Next(0, 2);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine(array[i]);
+    }
+}
+
+RandomArray(array);
+
+PrintArray(array);
