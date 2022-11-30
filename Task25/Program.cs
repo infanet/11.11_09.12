70eca3784c3b8ead93e9d8a91a165e1f39733db1
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-30 15:29:31 +0400

    Add Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,18 @@
+﻿int OutputToTheCube(int number, int degree)
+{
+    int n = number;
+    int sum = 1;
+    for (int i = 0; i < degree; i++)
+    {
+        sum = sum * n;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("В какую степень хотите возвести: ");
+int deg = Convert.ToInt32(Console.ReadLine());
+
+int outputToTheCube = OutputToTheCube(num, deg);
+Console.WriteLine($"{num} в степени {deg} будет -> {outputToTheCube}");
