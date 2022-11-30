3ca84b718f032dc456225ddf66167e7e01fa7b9b
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-30 16:25:22 +0400

    Add Task29

---
+++ Task29/Program.cs
@@ -0,0 +1,20 @@
+﻿int[] NumberOfArrayElements(int[] number)
+{
+    for (int i = 0; i < number.Length; i++)
+    {
+        Console.WriteLine($"Придай число к индексу {i}:");
+        number[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return number;
+}
+void OutputOfTheArrayNumber(int[] output)
+{
+    for (int i = 0; i < output.Length; i++)
+    {
+        Console.Write($"{output[i]} "); ; ;
+    }
+}
+Console.WriteLine("Укажи колиесто элемента массива:");
+int[] numberArray = new int[Convert.ToInt32(Console.ReadLine())];
+NumberOfArrayElements(numberArray);
+OutputOfTheArrayNumber(numberArray);
