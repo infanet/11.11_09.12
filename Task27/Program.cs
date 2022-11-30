7cd736eea73a904051a145e6c449d4a9003a6587
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-11-30 15:51:13 +0400

    Add Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,16 @@
+﻿int NumberSeparation(int number)
+{
+    int separation = default;
+    int sum = default;
+    while (number > 0)
+    {
+        separation = number % 10;
+        sum = sum + separation;
+        number = number / 10;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+int numberSeparation = NumberSeparation(num);
+Console.WriteLine($"Число {num} будет сложено между собой -> {numberSeparation}");
