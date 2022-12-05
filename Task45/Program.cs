a7f4fcb4f54ce2c980866eb19bfdc471c2efaa5b
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-05 17:41:19 +0400

    Task45

--- Task45/Program.cs
+++ Task45/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            System.Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            System.Console.Write($"{array[i]}");
+        }
+    }
+    Console.WriteLine($"]");
+}
+
+int[] CreateArray(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+    return arr;
+}
+
+int[] CopyArray(int[] arr)
+{
+    int[] tempArray = new int[arr.Length];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        tempArray[i] = arr[i];
+    }
+    return tempArray;
+}
+
+int[] arr = CreateArray(5, 1, 10);
+PrintArray(arr);
+int[] copyarr = CopyArray(arr);
+PrintArray(copyarr);
