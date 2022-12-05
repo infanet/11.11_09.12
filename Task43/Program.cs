39662661a97fe8caaf7c39abe333a6fc788c0d03
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-05 19:41:07 +0400

    Task43

--- Task43/Program.cs
+++ Task43/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double FindingsX(int b1, int k1, int b2, int k2)
+{
+    double withX = k1 + k2 * -1;
+    double withoutX = b2 + b1 * -1;
+    double x = withoutX / withX;
+    return x;
+}
+void FindingsY(int b1, int k1, int b2, int k2, double x)
+{
+    double B1 = b1;
+    double K1 = k1;
+    double B2 = b2;
+    double K2 = k2;
+    double y1 = K1 * x + B1;
+    double y2 = K2 * x + B2;
+    Console.WriteLine($"Точки пересеченя будут -> ({y1}; {y2})");
+}
+Console.WriteLine("y = k1*x + b1, y = k2*x + b2");
+Console.WriteLine("Введи число под b1: ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число под k1: ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число под b2: ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи число под k2: ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+double x = FindingsX(b1, k1, b2, k2);
+FindingsY(b1, k1, b2, k2, x);
