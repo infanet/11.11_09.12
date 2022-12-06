220d28c9bfee9199bef4ea949ae9f95510bc3909
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-06 16:54:43 +0400

    Task49

---
+++ Task49/Program.cs
@@ -0,0 +1,48 @@
+﻿int[,] CreateMatrixRndInt(int rows,int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write(matrix[i, j]);
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void SquareEvenNumbersMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                matrix[i, j] *= matrix[i, j];
+            }
+        }
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
+SquareEvenNumbersMatrix(array2D);
+PrintMatrix(array2D);
