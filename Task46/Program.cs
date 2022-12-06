3e4920cd3f7803c3c7158cdb0c93df490c24891c
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-06 15:40:16 +0400

    Task46

---
+++ Task46/Program.cs
@@ -0,0 +1,32 @@
+﻿int[,] CreateMatrixRndInt(int rows,int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, columns];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int y = 0; y < matrix.GetLength(1); y++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.WriteLine("[");
+        for (int y = 0; y < matrix.GetLength(1); y++)
+        {
+            if(j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write(matrix[i, j]);
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
+PrintMatrix(array2D);
