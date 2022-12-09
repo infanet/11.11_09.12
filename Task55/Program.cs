af889f5f3d1cf465fffe8a506251f73dca4072f4
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-09 13:23:35 +0400

    Task55

--- Task55/Program.cs
+++ Task55/Program.cs
@@ -1,2 +1,54 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write(matrix[i, j]);
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] CreateModifiedMatrix(int[,] matrix)
+{
+    int[,] matrix2 = new int[matrix.GetLength(0), matrix.GetLength(1)];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix2[i, j] = matrix[j, i];
+        }
+    }
+    return matrix2;
+}
+
+int[,] array2D = CreateMatrixRndInt(4, 4, 0, 5);
+PrintMatrix(array2D);
+int[,] matrix = CreateModifiedMatrix(array2D);
+if (array2D.GetLength(0) == array2D.GetLength(1))
+{
+    PrintMatrix(matrix);
+}
+else
+{
+    Console.WriteLine("Замена не возможна");
+}
