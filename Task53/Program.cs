9c401dc89bf0750f6f5a22992a5595745eb6a1ff
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-09 12:57:26 +0400

    Task53

---
+++ Task53/Program.cs
@@ -0,0 +1,46 @@
+﻿int[,] CreateMatrixRndInt(int rows, int colums, int min, int max)
+{
+    int[,] matrix = new int[rows, colums];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    Console.WriteLine();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j]}, ");
+            else Console.Write(matrix[i, j]);
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] ReplaceRows(int[,] matrix)
+{
+    int temp = 0;
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        temp = matrix[0, j];
+        matrix[0, j] = matrix[matrix.GetLength(0) - 1, j];
+        matrix[matrix.GetLength(0) - 1, j] = temp;
+    }
+    return matrix;
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, 0, 5);
+PrintMatrix(array2D);
+ReplaceRows(array2D);
+PrintMatrix(array2D);
