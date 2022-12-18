ef7a55dfa552986c88c04e277d921a35db615c13
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-18 18:59:57 +0400

    Add Task63

---
+++ Task63/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Введите натуральное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num)
+{
+    if (num == 0)
+    {
+        return;
+    }
+    Console.Write($"{num} ");
+    NaturalNumbers(num - 1);
+    Console.Write($"{num} ");
+}
+NaturalNumbers(number);
