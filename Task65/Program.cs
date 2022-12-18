5f319ea07a68defb2e5fa2b89125fb9317c70ba3
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-18 20:02:15 +0400

    Add Task65

---
+++ Task65/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+void NaturalNumbers(int num1, int num2)
+{
+    if (num1 < num2)
+    {
+        Console.Write($"{num1} ");
+        NaturalNumbers(num1 + 1, num2);
+    }
+    else if(num1 > num2)
+    {
+        NaturalNumbers(num1 - 1, num2);
+        Console.Write($"{num1} ");
+    }
+    else
+    {
+        Console.Write($"{num1} ");
+    }
+}
+NaturalNumbers(number1, number2);
