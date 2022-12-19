50df24c4cd727615882ca79a0d36a8da1112a598
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-20 00:06:45 +0400

    Task68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int Ackerman(int numM, int numN)
+{
+    if (numM == 0)
+    {
+        return numN + 1;
+    }
+    else if (numN == 0) return Ackerman(numM - 1, 1);
+    else return Ackerman(numM - 1, Ackerman(numM, numN - 1));
+}
+Console.WriteLine(Ackerman(3, 1));
