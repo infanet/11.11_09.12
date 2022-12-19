8d88122cb19bc17c12facc3b496c5058bac61f8f
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-19 15:08:59 +0400

    Add Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+void OutputOfNaturalNumbers(int num)
+{
+    if (num != 1)
+    {
+        Console.Write($"{num} ");
+        OutputOfNaturalNumbers(num - 1);
+    }
+    else
+    {
+        Console.Write($"{num} ");
+    }
+}
+OutputOfNaturalNumbers(number);
