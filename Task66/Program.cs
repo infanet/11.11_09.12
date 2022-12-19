03fd3299a1444cf30b85f10d7e7bb321f3ba9e31
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-19 15:24:03 +0400

    Add Task66

--- Task66/Program.cs
+++ Task66/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите первое число: ");
+int numberM = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int numberN = Convert.ToInt32(Console.ReadLine());
+
+int SumOfNaturalNumbers(int numM, int numN)
+{
+    if (numM != numN)
+    {
+        return numM + SumOfNaturalNumbers(numM + 1, numN);
+    }
+    else return numM;
+}
+Console.WriteLine(SumOfNaturalNumbers(numberM, numberN));
