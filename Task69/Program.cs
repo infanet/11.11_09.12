629745c7869d7c88186721e477c7ba42a16466ee
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-19 14:36:30 +0400

    Add Task69

--- Task69/Program.cs
+++ Task69/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+int SumDigits(int num1, int num2)
+{
+    return num2 == 0 ? 1 : SumDigits(num1, num2 - 1) * num1;
+}
+Console.WriteLine(SumDigits(number1, number2));
