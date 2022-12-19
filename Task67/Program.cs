34e3672f396d13d174f0938cad604eb67325e60f
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-19 14:18:29 +0400

    Add Task67

---
+++ Task67/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+int SumDigits(int num)
+{
+    return num != 0 ? num % 10 + SumDigits(num / 10) : 0;
+}
+Console.WriteLine(SumDigits(number1));
