42dc81d0f78305536581494551ee235eb8a492a8
Anton Klepikovemail <antosha_klepikov@bk.ru>
2022-12-24 21:14:33 +0400

    Task68

--- Task68/Program.cs
+++ Task68/Program.cs
@@ -1,16 +1,11 @@
-﻿Console.WriteLine("Введите первое число: ");
-int numberM = Convert.ToInt32(Console.ReadLine());
-
-Console.WriteLine("Введите второе число: ");
-int numberN = Convert.ToInt32(Console.ReadLine());
-
-int Ackerman(int numM, int numN)
+﻿int AckermanСalculation(int m, int n)
 {
-    if (numM == 0)
-    {
-        return numN + 1;
-    }
-    else if (numN == 0) return Ackerman(numM - 1, 1);
-    else return Ackerman(numM - 1, Ackerman(numM, numN - 1));
+    if (n == 0) return AckermanСalculation(m - 1, n + 1);
+    else if (m == 0) return n + 1;
+    else return AckermanСalculation(m - 1, AckermanСalculation(m, n - 1));
 }
-Console.WriteLine(Ackerman(3, 1));
+Console.WriteLine("Введи первое число для Аккермана: ");
+int ackermanNumbersM = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введи второе число для Аккермана: ");
+int ackermanNumbersN = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"\n" + AckermanСalculation(ackermanNumbersM, ackermanNumbersN));
